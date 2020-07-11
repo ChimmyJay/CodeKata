0820a8e3ccabbcb2695169dca12990c7a59ee247
Jay <Jay.hsieh@titansoft.com.sg>
2020-07-12 02:06:47 +0800

    Add test project

---
+++ CodeKata.UnitTest/CustomSourceTemplates.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+
+namespace CodeKata.UnitTest
+{
+    public static class CustomSourceTemplates
+    {
+        [JetBrains.Annotations.SourceTemplateAttribute]
+        [Macro(Target = "expected", Editable = 1)]
+        public static void ae<T>(this T source)
+        {
+            /*$ Assert.AreEqual( $expected$ , source);*/
+        }
+
+        [SourceTemplate]
+        public static void abs<T>(this T source)
+        {
+            /*$ Math.Abs(source)*/
+        }
+    }
+}
