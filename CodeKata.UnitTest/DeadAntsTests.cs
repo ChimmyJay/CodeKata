a52917a017c6ac9f7fe862579310af03496f7f90
Jay <Jay.hsieh@titansoft.com.sg>
2020-07-29 00:34:34 +0800

    Pass test

---
+++ CodeKata.UnitTest/DeadAntsTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace CodeKata.UnitTest
+{
+    public class DeadAntsTests
+    {
+        private DeadAnts _deadAnts;
+
+        [Test]
+        public void return_0_when_input_is_invalid()
+        {
+            Assert.AreEqual(0, _deadAnts.DeadAntCount(null));
+            Assert.AreEqual(0, _deadAnts.DeadAntCount(""));
+        }
+
+        [Test]
+        public void return_dead_ants_counts_when_input_valid()
+        {
+            var deadAnts = new DeadAnts();
+            Assert.AreEqual(0, deadAnts.DeadAntCount("ant ant ant ant"));
+            Assert.AreEqual(2, deadAnts.DeadAntCount("ant anantt aantnt"));
+            Assert.AreEqual(1, deadAnts.DeadAntCount("ant ant .... a nt"));
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _deadAnts = new DeadAnts();
+        }
+    }
+}
---
+++ CodeKata/DeadAnts.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace CodeKata
+{
+    public class DeadAnts
+    {
+        public int DeadAntCount(string ants)
+        {
+            if (string.IsNullOrEmpty(ants))
+            {
+                return 0;
+            }
+
+            var deadAnts = RemoveSurvivingAnts(ants);
+            var headCount = deadAnts.Count(x => x == 'a');
+            var bodyCount = deadAnts.Count(x => x == 'n');
+            var assCount = deadAnts.Count(x => x == 't');
+            var deadAntCount = new[] {headCount, bodyCount, assCount}.Max();
+
+            return deadAntCount;
+        }
+
+        private static string RemoveSurvivingAnts(string ants)
+        {
+            var deadAnts = ants.Replace("ant", "");
+            return deadAnts;
+        }
+    }
+}
